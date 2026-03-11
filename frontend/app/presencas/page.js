5517de9d9cd0cec456e71537d1fd269d7732e35e
ramonciro <ramonciro@gmail.com>
2026-03-11 15:47:35 -0300

    Create page.js

---
+++ frontend/app/presencas/page.js
@@ -0,0 +1,8 @@
+export default function PresencasPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Presenças</h1>
+      <p>Página de registro de presença dos treinamentos.</p>
+    </div>
+  );
+}
