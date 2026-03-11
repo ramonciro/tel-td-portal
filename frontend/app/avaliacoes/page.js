f213ee9f5cfee12f45dca0585d8e62e37c55b5d5
ramonciro <ramonciro@gmail.com>
2026-03-11 16:13:21 -0300

    Create page.js

---
+++ frontend/app/avaliacoes/page.js
@@ -0,0 +1,8 @@
+export default function AvaliacoesPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Avaliações</h1>
+      <p>Registro de NPS e qualidade dos treinamentos.</p>
+    </div>
+  );
+}
