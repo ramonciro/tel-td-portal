ab6de17a3cce58057ef18452d9dd08eb6464a622
ramonciro <ramonciro@gmail.com>
2026-03-11 14:33:23 -0300

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { login } from "../controllers/authController.js";
 import { getDashboard } from "../controllers/dashboardController.js";
-import { listUsers } from "../controllers/usersController.js";
+import { listUsers, creatUser } from "../controllers/usersController.js";
 import { listClientes } from "../controllers/clientesController.js";
 import { listTreinamentos, createTreinamento } from "../controllers/treinamentosController.js";
 import { authRequired } from "../middlewares/auth.js";
@@ -11,8 +11,10 @@ const router = Router();
 router.post("/auth/login", login);
 router.get("/dashboard", authRequired, getDashboard);
 router.get("/users", authRequired, listUsers);
+router.post("/users", authRequired, creatUser);
 router.get("/clientes", authRequired, listClientes);
 router.get("/treinamentos", authRequired, listTreinamentos);
 router.post("/treinamentos", authRequired, createTreinamento);
+
 
 export default router;
