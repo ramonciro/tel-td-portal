16b79fa717305cf6c58f61292bc6a438645b566b
ramonciro <ramonciro@gmail.com>
2026-03-11 14:36:39 -0300

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -11,7 +11,7 @@ const router = Router();
 router.post("/auth/login", login);
 router.get("/dashboard", authRequired, getDashboard);
 router.get("/users", authRequired, listUsers);
-router.post("/users", authRequired, creatUser);
+router.post("/users", authRequired, createUser);
 router.get("/clientes", authRequired, listClientes);
 router.get("/treinamentos", authRequired, listTreinamentos);
 router.post("/treinamentos", authRequired, createTreinamento);
