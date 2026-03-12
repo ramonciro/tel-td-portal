655c11f3192359d7d43f48053a5df0871ff80b70
ramonciro <ramonciro@gmail.com>
2026-03-12 12:04:57 -0300

    Update page.js

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,8 +1,12 @@
-export default function DashboardPage() {
+"use client";
+
+export default function Dashboard() {
+
   return (
-    <div style={{ padding: 40 }}>
-      <h1>Dashboard</h1>
-      <p>Teste estático.</p>
+    <div style={{padding:40}}>
+      <h1>Dashboard Teste</h1>
+      <p>Se você está vendo isso, o frontend está funcionando.</p>
     </div>
-  );
+  )
+
 }
