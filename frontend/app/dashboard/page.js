33bb2d6e23c0134d7e3944000d2913fcfd24eabc
ramonciro <ramonciro@gmail.com>
2026-03-11 22:29:58 -0300

    Update page.js

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,8 +1,8 @@
-export default funcition DashboardPage(){
-return (
-  <div style={{ padding:40 }}>
-<h1>Dashboard</h1>
-<p>Página de Teste do Dashboard.</p>
-  </div>
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Dashboard</h1>
+      <p>Página de teste do dashboard.</p>
+    </div>
   );
 }
