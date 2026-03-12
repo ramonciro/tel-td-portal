43b3106a800ef4ebd5dfe1ce6545a00b1aaa4446
ramonciro <ramonciro@gmail.com>
2026-03-12 14:23:39 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import router from "./routes/index.js";
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 app.use("/api", router);
 
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Tel T&D Backend rodando na porta ${PORT}`);
 });
