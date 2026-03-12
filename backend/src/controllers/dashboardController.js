8ceb111d13fbb590dde51de2917e20a4529e7c95
ramonciro <ramonciro@gmail.com>
2026-03-12 15:22:25 -0300

    Update dashboardController.js

--- backend/src/controllers/dashboardController.js
+++ backend/src/controllers/dashboardController.js
@@ -1,6 +1,6 @@
-const db = require("../config/db");
+import db from "../config/db.js";
 
-exports.getDashboard = async (req, res) => {
+export const getDashboard = async (req, res) => {
   try {
 
     const [treinamentos] = await db.query(`
@@ -16,35 +16,35 @@ exports.getDashboard = async (req, res) => {
     `);
 
     const [horas] = await db.query(`
-      SELECT SUM(carga_horaria) as horasTreinadas FROM treinamentos
+      SELECT IFNULL(SUM(carga_horaria),0) as horasTreinadas FROM treinamentos
     `);
 
     const [participantes] = await db.query(`
-      SELECT SUM(participantes_presentes) as participantesTreinados FROM treinamentos
+      SELECT IFNULL(SUM(participantes_presentes),0) as participantesTreinados FROM treinamentos
     `);
 
     const [conclusao] = await db.query(`
       SELECT 
-      SUM(concluidos) / NULLIF(SUM(participantes_presentes),0) * 100 as taxaConclusao
+      IFNULL((SUM(concluidos) / NULLIF(SUM(participantes_presentes),0)) * 100,0) as taxaConclusao
       FROM treinamentos
     `);
 
     const [aproveitamento] = await db.query(`
-      SELECT AVG(nota_prova) as mediaAproveitamento FROM avaliacoes
+      SELECT IFNULL(AVG(nota_prova),0) as mediaAproveitamento FROM avaliacoes
     `);
 
     res.json({
-      treinamentos: treinamentos[0].totalTreinamentos || 0,
-      usuarios: usuarios[0].totalUsuarios || 0,
-      clientes: clientes[0].totalClientes || 0,
-      horasTreinadas: horas[0].horasTreinadas || 0,
-      participantes: participantes[0].participantesTreinados || 0,
-      taxaConclusao: conclusao[0].taxaConclusao || 0,
-      aproveitamento: aproveitamento[0].mediaAproveitamento || 0
+      treinamentos: treinamentos[0].totalTreinamentos,
+      usuarios: usuarios[0].totalUsuarios,
+      clientes: clientes[0].totalClientes,
+      horasTreinadas: horas[0].horasTreinadas,
+      participantes: participantes[0].participantesTreinados,
+      taxaConclusao: conclusao[0].taxaConclusao,
+      aproveitamento: aproveitamento[0].mediaAproveitamento
     });
 
   } catch (error) {
-    console.error(error);
+    console.error("Erro dashboard:", error);
     res.status(500).json({ error: "Erro ao carregar dashboard" });
   }
 };
