3f8632f75cd8021ee0c623b9dd909f3c0fc4997e
ramonciro <ramonciro@gmail.com>
2026-03-12 11:29:37 -0300

    Update page.js

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,17 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Dashboard Tel T&D</h1>
-
-      <p>Portal de Treinamento e Desenvolvimento</p>
-
-      <div style={{ marginTop: 30 }}>
-        <p>✔ Sistema operacional</p>
-        <p>✔ Backend conectado</p>
-        <p>✔ Banco de dados ativo</p>
-      </div>
-    </div>
-  );
+export default function DashboardPage() {
+  redirect("/clientes");
 }
