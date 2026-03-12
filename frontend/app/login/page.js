2600d7548c3469394481d3f9a90c35dad2499d61
ramonciro <ramonciro@gmail.com>
2026-03-12 11:30:14 -0300

    Update page.js

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -38,7 +38,7 @@ export default function Login() {
       const data = await res.json();
 
       localStorage.setItem("token", data.token);
-      window.location.href = "/painel";
+      window.location.href = "/clientes";
     } catch {
       setErro("Erro ao conectar com o servidor");
     }
