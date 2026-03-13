239c3b8c077a12733b8f0f335a173ed2672baa84
ramonciro <ramonciro@gmail.com>
2026-03-13 15:00:37 -0300

    Add files via upload

--- frontend/app/mapa-desenvolvimento/page.js
+++ frontend/app/mapa-desenvolvimento/page.js
@@ -1,5 +1,13 @@
 "use client";
-import CrudPage from "../../components/CrudPage"
+
+import CrudPage from "../../components/CrudPage";
+
 export default function Page() {
-  return <CrudPage title="Mapa de Desenvolvimento" subtitle="Evolu\u00e7\u00e3o pessoal e profissional cont\u00ednua" endpoint="/mapa-desenvolvimento" fields=[{"name": "colaborador", "label": "Colaborador"}, {"name": "cliente", "label": "Cliente"}, {"name": "cargo", "label": "Cargo"}, {"name": "objetivo_profissional", "label": "Objetivo Profissional", "type": "textarea"}, {"name": "trilha_atual", "label": "Trilha Atual"}, {"name": "etapa_atual", "label": "Etapa Atual"}, {"name": "status", "label": "Status"}, {"name": "percentual", "label": "Percentual", "type": "number"}, {"name": "proximo_passo", "label": "Próximo Passo", "type": "textarea"}, {"name": "mentor", "label": "Mentor"}, {"name": "observacoes", "label": "Observações", "type": "textarea"}] />
+  return (
+    <CrudPage
+      title="Mapa de Desenvolvimento"
+      subtitle="Evolução pessoal e profissional contínua"
+      endpoint="/mapa-desenvolvimento"
+    />
+  );
 }
