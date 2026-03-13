40e8d45091812d0e6f5fc2defd3c55d926605f1f
ramonciro <ramonciro@gmail.com>
2026-03-13 12:51:08 -0300

    Add files via upload

---
+++ dashboardRoutes.js
@@ -0,0 +1,26 @@
+
+const express = require("express")
+const cors = require("cors")
+
+const dashboardRoutes = require("./routes/dashboardRoutes")
+const usuariosRoutes = require("./routes/usuariosRoutes")
+const clientesRoutes = require("./routes/clientesRoutes")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/api",(req,res)=>{
+  res.json({status:"API Tel T&D online"})
+})
+
+app.use("/api/dashboard",dashboardRoutes)
+app.use("/api/usuarios",usuariosRoutes)
+app.use("/api/clientes",clientesRoutes)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+  console.log("Servidor rodando na porta",PORT)
+})
