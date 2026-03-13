a549607994d14c65bbe9ad52621d61c80a441a50
ramonciro <ramonciro@gmail.com>
2026-03-13 14:58:29 -0300

    Add files via upload

--- frontend/app/avaliacoes/page.js
+++ frontend/app/avaliacoes/page.js
@@ -1,5 +1,13 @@
 "use client";
-import CrudPage from "../../components/CrudPage"
+
+import CrudPage from "../../components/CrudPage";
+
 export default function Page() {
-  return <CrudPage title="Avalia\u00e7\u00f5es" subtitle="Registro de NPS, qualidade e provas" endpoint="/avaliacoes" fields=[{"name": "treinamento_id", "label": "Treinamento ID", "type": "number"}, {"name": "titulo", "label": "Título"}, {"name": "nota_nps", "label": "Nota NPS", "type": "number"}, {"name": "nota_qualidade", "label": "Nota Qualidade", "type": "number"}, {"name": "nota_prova", "label": "Nota Prova", "type": "number"}, {"name": "observacoes", "label": "Observações", "type": "textarea"}] />
+  return (
+    <CrudPage
+      title="Avaliações"
+      subtitle="Registro de NPS, qualidade e provas"
+      endpoint="/avaliacoes"
+    />
+  );
 }
