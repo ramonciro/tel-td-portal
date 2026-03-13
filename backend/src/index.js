dca11424fb79e5928cfa091380cc70cc7af02d22
ramonciro <ramonciro@gmail.com>
2026-03-13 13:07:43 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,18 +11,17 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-
-app.get("/api", (req,res)=>{
-  res.json({status:"API Tel T&D online"})
+app.get("/api", (req, res) => {
+  res.json({ status: "API Tel T&D online" })
 })
 
 app.use("/api/dashboard", dashboardRoutes)
 app.use("/api/usuarios", usuariosRoutes)
 app.use("/api/clientes", clientesRoutes)
-app.use("/api",authRoutes)
+app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log("Servidor rodando na porta", PORT)
 })
