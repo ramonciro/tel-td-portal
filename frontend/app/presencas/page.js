8e7451a31bcd04f893e07ebea6e87784deaa3fac
ramonciro <ramonciro@gmail.com>
2026-03-13 15:00:52 -0300

    Add files via upload

--- frontend/app/presencas/page.js
+++ frontend/app/presencas/page.js
@@ -1,5 +1,13 @@
 "use client";
-import CrudPage from "../../components/CrudPage"
+
+import CrudPage from "../../components/CrudPage";
+
 export default function Page() {
-  return <CrudPage title="Presen\u00e7as" subtitle="Controle de presen\u00e7a, aus\u00eancia e justificativas" endpoint="/presencas" fields=[{"name": "treinamento_id", "label": "Treinamento ID", "type": "number"}, {"name": "treinando_nome", "label": "Nome do Treinando"}, {"name": "status", "label": "Status", "type": "select", "options": ["presente", "ausente", "justificado"]}, {"name": "justificativa", "label": "Justificativa", "type": "textarea"}] />
+  return (
+    <CrudPage
+      title="Presenças"
+      subtitle="Controle de presença, ausência e justificativas"
+      endpoint="/presencas"
+    />
+  );
 }
