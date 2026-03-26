ea639111745fc5cac95088999fe25d76b51d83ea
ramonciro <ramonciro@gmail.com>
2026-03-26 11:19:16 -0300

    Create coachingPlanosRoutes.js

---
+++ backend/src/routes/coachingPlanosRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/coachingPlanosController");
+
+router.get("/", controller.listar);
+router.get("/:id", controller.buscarPorId);
+router.post("/", controller.criar);
+router.put("/:id", controller.atualizar);
+router.delete("/:id", controller.remover);
+
+module.exports = router;
