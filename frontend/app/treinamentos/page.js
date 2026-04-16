e5400258e2715d4aa59ad8089c8bf63be91f8b3a
ramonciro <ramonciro@gmail.com>
2026-04-16 13:45:58 -0300

    Update page.js

--- frontend/app/treinamentos/page.js
+++ frontend/app/treinamentos/page.js
@@ -162,13 +162,9 @@ export default function TreinamentosPage() {
       return lista;
     }
 
-if (perfilLogado === "instrutor" || perfilLogado === "supervisor") {
-  const filtrados = lista.filter((item) =>
-    temClienteEmComum(item.value, clienteLogado)
-  );
-
-  return filtrados.length ? filtrados : lista;
-}
+    if (perfilLogado === "instrutor" || perfilLogado === "supervisor") {
+      return lista.filter((item) => temClienteEmComum(item.value, clienteLogado));
+    }
 
     return lista;
   }, [clientes, perfilLogado, usuarioEhGlobal, clienteLogado]);
@@ -244,11 +240,8 @@ if (perfilLogado === "instrutor" || perfilLogado === "supervisor") {
         instrutores.length > 0
           ? "Selecione o instrutor"
           : "Nenhum instrutor disponível",
-      defaultValue:
-  perfilLogado === "instrutor" && instrutores.length
-    ? instrutores[0].value
-    : "",
-      disabled: false,
+      defaultValue: perfilLogado === "instrutor" ? nomeLogado : "",
+      disabled: perfilLogado === "instrutor",
     },
     {
       name: "supervisor",
